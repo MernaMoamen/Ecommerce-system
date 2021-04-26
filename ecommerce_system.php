7b2c9231935db4d363e184ed94f259edfd21e36d
MernaMoamen <75040325+MernaMoamen@users.noreply.github.com>
2021-04-26 20:02:57 +0200

    Add files via upload

---
+++ ecommerce_system.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.0.4
+ */
+
+/**
+ * Database `ecommerce system`
+ */
+
+/* `ecommerce system`.`admin` */
+$admin = array(
+  array('ID' => '1','name' => 'merna mohsen','password' => '2001')
+);
+
+/* `ecommerce system`.`customers` */
+$customers = array(
+  array('customerID' => '1','name' => 'ahmed','email' => 'ahmed@gmail.com','password' => '123'),
+  array('customerID' => '2','name' => 'jana','email' => 'jana987@hotmail.com','password' => '345')
+);
+
+/* `ecommerce system`.`employees` */
+$employees = array(
+  array('ID' => '1','name' => 'laila','email' => 'laila123@gmail.com','password' => '1234'),
+  array('ID' => '2','name' => 'ahmed','email' => 'ahmed345@gmail.com','password' => '3456')
+);
+
+/* `ecommerce system`.`orders` */
+$orders = array(
+  array('ordID' => '22','prodID' => '12')
+);
+
+/* `ecommerce system`.`products` */
+$products = array(
+  array('ID' => '12','name' => 'Apple Iphone 11','details' => ' FaceTime - 128GB, 4GB RAM, 4G LTE, Green, Single SIM & E-SIM - MHDN3AAA','price' => '12100','image' => 'iphone11.jpg'),
+  array('ID' => '13','name' => 'Redmi Note 10 ','details' => 'Dual SIM - 6.43 Inches, 64 GB, 4 GB RAM, 4G LTE - Onyx Gray','price' => '2910','image' => 'item_XL_132495380_eab1b681c6223.jpg'),
+  array('ID' => '14','name' => 'Huawei P40 ','details' => 'Dual Sim - 128GB, 8GB RAM, 5G, Black','price' => '9999','image' => 'item_XXL_131532029_f95977ef75cd6.jpg')
+);
